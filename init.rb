5251020ca9413c248ff0ee68acd49f71aa8252aa
Matt Allen <matt.allen@gmail.com>
2009-04-08 13:30:34 +1000

    README fixes, load path fixes

--- init.rb
+++ init.rb
@@ -1,13 +1,13 @@
 require 'net/http'
 require 'uri'
 
-helper_path = File.join(RAILS_ROOT, 'vendor', 'plugins','cached_partials','helpers','cached_partials_helper')
+helper_path = File.join(RAILS_ROOT, 'vendor', 'plugins','cached-partials','helpers','cached_partials_helper')
 require helper_path
 
 ActiveRecord::Base.send :include, CachedPartials
 ActionView::Base.send :include, CachedPartialsHelper
 
-controller_path = File.join(RAILS_ROOT, 'vendor', 'plugins','cached_partials','controllers')
+controller_path = File.join(RAILS_ROOT, 'vendor', 'plugins','cached0partials','controllers')
 $LOAD_PATH << controller_path
 ActiveSupport::Dependencies.load_paths << controller_path
 config.controller_paths << controller_path
